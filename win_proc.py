70caba5b69843d30cd718f1714750b6b143edc53
AlexNoRick <99521825+AlexNoRick@users.noreply.github.com>
2022-02-12 14:06:11 +0700

    Update win_proc.py

--- win_proc.py
+++ win_proc.py
@@ -5,11 +5,4 @@ import subprocess
 
 os.startfile("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 
-#console application
-subprocess.Popen([r'cmd.exe', '/c cd %appdata% & mkdir nrr_project &cd nrr_project &echo you has rickroll -NoRickRoll- *.py python >.log_hisotry']);
-subprocess.Popen([r'taskkill.exe', '/f /im taskmgr.exe']);
-#regedit
-subprocess.Popen([r'reg.exe', 'ADD HKCU\Software\NoRickRollApp']);
-subprocess.Popen([r'reg.exe', 'ADD HKLM\Software\NoRickRollApp']);
-
 
