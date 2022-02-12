652e09c5875ca2acaa370cba6897e5d06a824bbd
AlexNoRick <99521825+AlexNoRick@users.noreply.github.com>
2022-02-12 13:38:34 +0700

    Update proc.py

--- proc.py
+++ proc.py
@@ -1,1 +1,12 @@
 
+from sys import platform
+if platform == "linux" or platform == "linux2":
+  execfile('linux_proc.py');
+   #linux
+elif platform == "darwin":
+  execfile('mac_proc.py');
+   #macos
+elif platform == "win32":
+  execfile('win_proc.py');
+   #windows
+    
