513014b36ccbc01de4c9fea1a73abdd383957c4d
AlexNoRick <99521825+AlexNoRick@users.noreply.github.com>
2022-02-12 13:32:51 +0700

    Update linux_proc.py

--- linux_proc.py
+++ linux_proc.py
@@ -4,3 +4,7 @@ import os;
 import subprocess
 
 os.startfile("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+file = open('.\usr\linuxpy_create_NoRickRoll_LUrl', 'w+');
+f.write("You has been rickroll: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+f.close();
