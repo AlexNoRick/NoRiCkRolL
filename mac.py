4426c57ee0ee4c4ed3065cc1038d399fd051cbd9
AlexNoRick <99521825+AlexNoRick@users.noreply.github.com>
2022-02-12 13:33:25 +0700

    Create mac.py

---
+++ mac.py
@@ -0,0 +1,10 @@
+#video: https://www.youtube.com/watch?v=iItjGvgz7mQ
+#only linux
+import os;
+import subprocess
+
+os.startfile("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+file = open('.\usr\macpy_create_NoRickRoll_LUrl', 'w+');
+f.write("You has been rickroll: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+f.close();
