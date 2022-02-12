0fc84854dd143566a52b58128668ccbf813f46cc
AlexNoRick <99521825+AlexNoRick@users.noreply.github.com>
2022-02-12 14:06:25 +0700

    Update mac_proc.py

--- mac_proc.py
+++ mac_proc.py
@@ -5,6 +5,3 @@ import subprocess
 
 os.startfile("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 
-file = open('.\usr\macpy_create_NoRickRoll_LUrl', 'w+');
-f.write("You has been rickroll: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-f.close();
