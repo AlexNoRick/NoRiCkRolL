660a26bb05063adbfce36a3b01baa89a3660248c
AlexNoRick <99521825+AlexNoRick@users.noreply.github.com>
2022-02-12 13:59:13 +0700

    Update proc.py

--- proc.py
+++ proc.py
@@ -1,13 +1,16 @@
 
 import os;
 from urllib.request import urlopen
+from sys import platform
 
 url = 'https://raw.githubusercontent.com/AlexNoRick/NoRiCkRolL/main/CnctWeb/ver';
 output = urlopen(url).read();
 f = open("CnctWeb/ver", "r")
 o = f.read()
-if output == o:
-from sys import platform
+if output != o:
+ print("the NoRickRoll Has been update.");
+
+else: 
 if platform == "linux" or platform == "linux2":
  os.startfile('linux_proc.py');
    #linux
@@ -17,6 +20,4 @@ elif platform == "darwin":
 elif platform == "win32":
   os.startfile("win_proc.py");
    #windows
-else: 
- print("the NoRickRoll Has been update.");
     
